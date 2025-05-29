977f9e2a86b5f998a4104a2cc46874fdb3723511
vnphu <vonhuphu117@gmail.com>
2025-05-29 13:09:27 +0700

    style:

--- components/common/ButtonFilter.vue
+++ components/common/ButtonFilter.vue
@@ -21,12 +21,12 @@ const hidePanel = () => {
 
 <template>
   <Button
-    class="gap-2 group bg-primary-20 !hover:bg-primary-20 border-transparent overflow-visible"
+    class="gap-2 group bg-primary !hover:bg-primary-20 border-transparent overflow-visible"
     v-badge.info="numberBadge > 0 ? numberBadge.toString() : undefined"
     @click="toggle"
   >
-    <img src="~/assets/icons/i-filter-secondary.svg" class="w-[19px] h-[19px]" alt="" />
-    <span class="c-primary"> {{ t('button.filter') }} </span>
+    <img src="~/assets/icons/i-filter-white.svg" class="w-[19px] h-[19px]" alt="" />
+    <span class="!c-white"> {{ t('button.filter') }} </span>
   </Button>
 
   <OverlayPanel ref="op">
